4423924de6ee76438d8c433db0b28bbba70f6ef8
Gang Shao <103646270+DrShaoGang@users.noreply.github.com>
2022-09-08 16:46:25 -0400

    Network update with enlarged text label

--- BoW_network.py
+++ BoW_network.py
@@ -12,6 +12,10 @@ from pyvis.network import Network
 import pandas as pd
 import streamlit as st
 from operator import itemgetter
+import requests
+import regex
+from bs4 import BeautifulSoup as soup
+from math import log
 
 #read in data
 tags_df = pd.read_csv('bag_of_words(1).csv')
@@ -42,8 +46,8 @@ sorted_nodes = sorted(node_info, key = itemgetter(2), reverse = True)
 
 def simple_func(): 
   #create graph as an instance of a Pyvis network
-  nx_graph = Network('500px', '500px', notebook = True, heading='') #array of three particles made, index starts at 0
-  st.markdown("<h1 style = 'font-size: 100px;'> </h1>")
+  nx_graph = Network(height="1000px", width = "100%", notebook = True, heading='PURR Tag Network') #array of three particles made, index starts at 0
+  st.markdown("<h1 style = 'font-size: 50px;'> </h1>", unsafe_allow_html= True)
   
   
   #create empty master and sub list
@@ -73,26 +77,42 @@ def simple_func():
   
   #This works for sure
   #normally 500
-  for initial in range(0, 300):
+  for initial in range(0, 200):
+          
      tag = sorted_nodes[initial][0]
-     node_size = sorted_nodes[initial][2]
-     nx_graph.add_node(tag, tag, title = tag, size = node_size)
-  
+     node_size = 3 * log(sorted_nodes[initial][2])
+     # nx_graph.add_node(tag, tag, title = tag, size = node_size)
+     
+     #for each of the top 200 nodes, add search term
+     
+     #replace snake case with +
+     tag_plus = regex.sub("_", "+", tag)
+     tag_show = regex.sub("_", "_", tag)
+     
+     #HTML parse, get search results
+     #site_raw = requests.get("https://purr.lib.purdue.edu/registry?q=" + tag_plus, verify = False)
+     #site_soup = soup(site_raw.text, 'html.parser')
+     #site_info = site_soup.find("p", {"class": "ml-2 mt-3"})
+     #results = regex.compile("(<p class=\"ml-2 mt-3\">)\s*((Results[\s\w-]+)|(No record found\s+))(<\/p>)") #regex that finds count of results
+     link = "https://purr.lib.purdue.edu/registry?q=" + tag_plus
+     hyperlink = "<a href="+link+">View PURR data in :  '" + tag_show + "'""</a>"
+     
+     nx_graph.add_node(tag, tag, font_size= 300, title = hyperlink, size = node_size)
   
   #get information for nodes, including source and source location
   #normally 499
-  for initial in range(0, 299):
+  for initial in range(0, 199):
     tag_init = sorted_nodes[initial][0]
     col_init = sorted_nodes[initial][1]
     
     #get destination and destination location, along with weight of edge
     #normally 500
-    for start in range(initial + 1, 300):
+    for start in range(initial + 1, 200):
         tag_value = sorted_nodes[start][0]
         col_value = sorted_nodes[start][1]
         node_weight = int(tags_df.iloc[col_init][col_value + 1])
         if node_weight != 0:
-          nx_graph.add_edge(tag_init, tag_value, weight = node_weight)
+          nx_graph.add_edge(tag_init, tag_value, weight = 1)#node_weight)
 
 
 
@@ -128,6 +148,7 @@ def simple_func():
   
     '''
   #show the graph at the end
-  nx_graph.show_buttons(filter_=["physics"])
+  #turn off the physics option
+  nx_graph.barnes_hut(gravity = -36000, central_gravity = 0, spring_length = 200, spring_strength = 0.04)
   nx_graph.show('BoW.html')
   
